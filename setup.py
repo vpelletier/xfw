3b2aa6e4be1d15c9614abbfdbf0443751fcc119f
Vincent Pelletier <vincent@nexedi.com>
2013-11-25 18:33:04 +0100

    setup.py: Use README.rst as long description.
    Also, declare zip-safety.
    Also, switch to setuptools.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+from os.path import join, dirname
+
+description = open(join(dirname(__file__), 'README.rst')).read()
 
 setup(
     name='xfw',
     version='0.9',
     description='eXtensible Fixed-Width file handling module',
+    long_description='.. contents::\n\n' + description,
     keywords='fixed width field file',
     author='Nexedi',
     author_email='erp5-dev@erp5.org',
@@ -16,5 +20,5 @@ setup(
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
     ],
+    zip_safe=True,
 )
-
